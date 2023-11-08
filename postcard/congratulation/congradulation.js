bd625aa52f4bf1c68955edb3f3de3ee960b3e7a9
Vlad Chelombitko <vladmorilov24@gmail.com>
2023-11-08 04:28:19 -0600

    Open-close images

---
+++ postcard/congratulation/congradulation.js
@@ -0,0 +1,259 @@
+const images1 = [
+    "../pictures/free-icon-gift-5990435.png",
+    "../pictures/free-icon-gift-box-837993.png",
+];
+
+let currentIndex = 0;
+let isForward = true;
+
+setInterval(function changeImage1() {
+    const mainImageElement = document.getElementById("image1");
+    mainImageElement.src = images1[currentIndex];
+    if (isForward) {
+        currentIndex++;
+    } else {
+        currentIndex--;
+    }
+
+    if (currentIndex === images1.length) {
+        currentIndex = images1.length - 2;
+        isForward = false;
+    } else if (currentIndex === -1) {
+        currentIndex = 1;
+        isForward = true;
+    }
+}, 1000);
+
+const images2 = [
+    "../pictures/free-icon-gift-5990435.png",
+    "../pictures/free-icon-gift-box-837993.png",
+];
+
+let currentIndex2 = 0;
+let isForward2 = true;
+
+setInterval(function changeImage2() {
+    const mainImageElement = document.getElementById("image2");
+    mainImageElement.src = images2[currentIndex2];
+    if (isForward2) {
+        currentIndex2++;
+    } else {
+        currentIndex2--;
+    }
+
+    if (currentIndex2 === images2.length) {
+        currentIndex2 = images2.length - 2;
+        isForward2 = false;
+    } else if (currentIndex2 === -1) {
+        currentIndex2 = 1;
+        isForward2 = true;
+    }
+}, 1000);
+
+const images3 = [
+    "../pictures/free-icon-gift-5990435.png",
+    "../pictures/free-icon-gift-box-837993.png",
+];
+
+let currentIndex3 = 0;
+let isForward3 = true;
+
+setInterval(function changeImage3() {
+    const mainImageElement = document.getElementById("image3");
+    mainImageElement.src = images3[currentIndex3];
+    if (isForward3) {
+        currentIndex3++;
+    } else {
+        currentIndex3--;
+    }
+
+    if (currentIndex3 === images3.length) {
+        currentIndex3 = images3.length - 2;
+        isForward3 = false;
+    } else if (currentIndex3 === -1) {
+        currentIndex3 = 1;
+        isForward3 = true;
+    }
+}, 1000);
+
+const images4 = [
+    "../pictures/free-icon-gift-5990435.png",
+    "../pictures/free-icon-gift-box-837993.png",
+];
+
+let currentIndex4 = 0;
+let isForward4 = true;
+
+setInterval(function changeImage4() {
+    const mainImageElement = document.getElementById("image4");
+    mainImageElement.src = images4[currentIndex4];
+    if (isForward4) {
+        currentIndex4++;
+    } else {
+        currentIndex4--;
+    }
+
+    if (currentIndex4 === images4.length) {
+        currentIndex4 = images4.length - 2;
+        isForward4 = false;
+    } else if (currentIndex4 === -1) {
+        currentIndex4 = 1;
+        isForward4 = true;
+    }
+}, 1000);
+
+const images5 = [
+    "../pictures/free-icon-gift-5990435.png",
+    "../pictures/free-icon-gift-box-837993.png",
+];
+
+let currentIndex5 = 0;
+let isForward5 = true;
+
+setInterval(function changeImage5() {
+    const mainImageElement = document.getElementById("image5");
+    mainImageElement.src = images5[currentIndex5];
+    if (isForward5) {
+        currentIndex5++;
+    } else {
+        currentIndex5--;
+    }
+
+    if (currentIndex5 === images5.length) {
+        currentIndex5 = images5.length - 2;
+        isForward5 = false;
+    } else if (currentIndex5 === -1) {
+        currentIndex5 = 1;
+        isForward5 = true;
+    }
+}, 1000);
+
+const images6 = [
+    "../pictures/free-icon-gift-5990435.png",
+    "../pictures/free-icon-gift-box-837993.png",
+];
+
+let currentIndex6 = 0;
+let isForward6 = true;
+
+setInterval(function changeImage6() {
+    const mainImageElement = document.getElementById("image6");
+    mainImageElement.src = images6[currentIndex6];
+    if (isForward6) {
+        currentIndex6++;
+    } else {
+        currentIndex6--;
+    }
+
+    if (currentIndex6 === images6.length) {
+        currentIndex6 = images6.length - 2;
+        isForward6 = false;
+    } else if (currentIndex6 === -1) {
+        currentIndex6 = 1;
+        isForward6 = true;
+    }
+}, 1000);
+
+const images7 = [
+    "../pictures/free-icon-gift-5990435.png",
+    "../pictures/free-icon-gift-box-837993.png",
+];
+
+let currentIndex7 = 0;
+let isForward7 = true;
+
+setInterval(function changeImage7() {
+    const mainImageElement = document.getElementById("image7");
+    mainImageElement.src = images7[currentIndex7];
+    if (isForward7) {
+        currentIndex7++;
+    } else {
+        currentIndex7--;
+    }
+
+    if (currentIndex7 === images7.length) {
+        currentIndex7 = images7.length - 2;
+        isForward7 = false;
+    } else if (currentIndex7 === -1) {
+        currentIndex7 = 1;
+        isForward7 = true;
+    }
+}, 1000);
+
+const images8 = [
+    "../pictures/free-icon-gift-5990435.png",
+    "../pictures/free-icon-gift-box-837993.png",
+];
+
+let currentIndex8 = 0;
+let isForward8 = true;
+
+setInterval(function changeImage8() {
+    const mainImageElement = document.getElementById("image8");
+    mainImageElement.src = images8[currentIndex8];
+    if (isForward8) {
+        currentIndex8++;
+    } else {
+        currentIndex8--;
+    }
+
+    if (currentIndex8 === images8.length) {
+        currentIndex8 = images8.length - 2;
+        isForward8 = false;
+    } else if (currentIndex8 === -1) {
+        currentIndex8 = 1;
+        isForward8 = true;
+    }
+}, 1000);
+
+const images9 = [
+    "../pictures/free-icon-gift-5990435.png",
+    "../pictures/free-icon-gift-box-837993.png",
+];
+
+let currentIndex9 = 0;
+let isForward9 = true;
+
+setInterval(function changeImage9() {
+    const mainImageElement = document.getElementById("image9");
+    mainImageElement.src = images9[currentIndex9];
+    if (isForward9) {
+        currentIndex9++;
+    } else {
+        currentIndex9--;
+    }
+
+    if (currentIndex9 === images9.length) {
+        currentIndex9 = images9.length - 2;
+        isForward9 = false;
+    } else if (currentIndex9 === -1) {
+        currentIndex9 = 1;
+        isForward9 = true;
+    }
+}, 1000);
+
+const images90 = [
+    "../pictures/free-icon-gift-5990435.png",
+    "../pictures/free-icon-gift-box-837993.png",
+];
+
+let currentIndex90 = 0;
+let isForward90 = true;
+
+setInterval(function changeImage90() {
+    const mainImageElement = document.getElementById("image90");
+    mainImageElement.src = images90[currentIndex90];
+    if (isForward90) {
+        currentIndex90++;
+    } else {
+        currentIndex90--;
+    }
+
+    if (currentIndex90 === images90.length) {
+        currentIndex90 = images90.length - 2;
+        isForward90 = false;
+    } else if (currentIndex90 === -1) {
+        currentIndex90 = 1;
+        isForward90 = true;
+    }
+}, 1000);
